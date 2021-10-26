470c4749c2e72757171daf35dbb02b4d63cf7fe5
JMZ100 <89689386+JMZ100@users.noreply.github.com>
2021-10-26 15:12:31 +0400

    Update testing.ino

--- testing.ino
+++ testing.ino
@@ -1,37 +1,37 @@
 
-#include <M5Core2.h>
-#include "MAX30100_PulseOximeter.h"
+#include <M5Core2.h> // M5CORE2 library
+#include "MAX30100_PulseOximeter.h" // Sensor header file
 
-#define REPORTING_PERIOD_MS     1000
-PulseOximeter pox;
-uint32_t tsLastReport = 0;
+#define REPORTING_PERIOD_MS     1000 //Time between screen updates
+PulseOximeter pox; //Sensor functions
+uint32_t tsLastReport = 0; 
 
-void onBeatDetected()
+void onBeatDetected() //Function prints if a heartbeat 
 {
     Serial.println("B:1");
 }
 
-void setup() {
-  M5.begin(true, false, true, true);  //Init M5Core2.  初始化 M5Core2
-  M5.Lcd.print("Welcome!");  // Print text on the screen (string) 在屏幕上打印文本(字符串)
-  Serial.print("It's on!");
+void setup() {  //One time executed functions
+  M5.begin(true, false, true, true);  //Initiate M5Core2.  
+  M5.Lcd.print("Welcome!");  // Print text on the M5 screen
+  Serial.print("It's on!"); //Print on ide if M5 is on
 
-  if (!pox.begin()) {
+  if (!pox.begin()) { // Check if the sensor is recognized
         Serial.println("ERROR: Failed to initialize pulse oximeter");
         M5.Lcd.println("ERROR: Failed to initialize pulse oximeter");
   } else {
         Serial.println("SUCCESS");
   }
 
-    pox.setOnBeatDetectedCallback(onBeatDetected);
+    pox.setOnBeatDetectedCallback(onBeatDetected); //Print heartbeat detected if it is working
 }
 
 
 
-void loop() {
-  pox.update();
+void loop() { //Repeatedly executed program 
+  pox.update(); //Retrieve next heartbeat value
 
-  if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS) { //Print heartbeat frequency in the screen and in the console
         M5.Lcd.clear();
         M5.Lcd.setCursor(0,0);
         M5.Lcd.setTextSize(5);
